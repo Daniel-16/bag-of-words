e14fdfd4d1787308a840b5b083b0c8df7ea55e97
Daniel Toba <danieloloruntoba681@gmail.com>
2026-02-07 16:48:13 +0100

    fix: api url

--- src/api.py
+++ src/api.py
@@ -45,6 +45,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:8080",
         "http://127.0.0.1:5173",
+        "https://bag-of-words.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
